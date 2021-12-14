e932da0d7b6d72686d01b4e3d31e01678226db7a
cry0x <kraemerj.schmelz@outlook.de>
2021-12-14 20:41:25 +0100

    some basic tests

--- product_service/src/test/java/org/msia_projekt/product_service/ProductServiceApplicationTests.java
+++ product_service/src/test/java/org/msia_projekt/product_service/ProductServiceApplicationTests.java
@@ -1,13 +1,29 @@
 package org.msia_projekt.product_service;
 
 import org.junit.jupiter.api.Test;
+import org.msia_projekt.product_service.controllers.ArticleController;
+import org.msia_projekt.product_service.repositories.IArticleRepository;
+import org.msia_projekt.product_service.services.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class ProductServiceApplicationTests {
 
+    @Autowired
+    private ArticleController articleController;
+    @Autowired
+    private ArticleService articleService;
+    @Autowired
+    private IArticleRepository iArticleRepository;
+
     @Test
     void contextLoads() {
+        assertNotNull(articleController);
+        assertNotNull(articleService);
+        assertNotNull(iArticleRepository);
     }
 
 }
