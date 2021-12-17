8c73b04fa6387a3d9d6766d0cdc01854b8ffd56d
MrKumata <n.Welsch@protonmail.com>
2021-12-17 09:07:37 +0100

    Initial Setup

---
+++ customer_service/src/main/java/com/customer_service/customer_service/CustomerServiceApplication.java
@@ -0,0 +1,18 @@
+package com.customer_service.customer_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
+@SpringBootApplication
+// See: localhost:8080/swagger-ui.html
+// Get JSON: http://localhost:8080/v3/api-docs
+@OpenAPIDefinition
+public class CustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServiceApplication.class, args);
+	}
+
+}
