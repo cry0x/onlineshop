8cb192541fc81b9e030e5d60f597e26f48f80992
jens_k <37385066+cry0x@users.noreply.github.com>
2022-01-08 23:15:15 +0100

    fix(docker-compose) added customer-service to docker-compose (#14)
    * first steps
    
    * wtf
    
    * wtf
    
    * removed arg from customer_service Dockerfile
    
    * fixed static images error
    
    * fixed docker-compose for all services
    
    * test
    
    * removed test
    
    * removed test

--- product_service/src/main/java/org/msia_projekt/product_service/utilities/DefaultProductPicture.java
+++ product_service/src/main/java/org/msia_projekt/product_service/utilities/DefaultProductPicture.java
@@ -3,7 +3,6 @@ package org.msia_projekt.product_service.utilities;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 
 public class DefaultProductPicture {
@@ -17,8 +16,7 @@ public class DefaultProductPicture {
 
         try {
             Resource resource = new ClassPathResource("static\\images\\default_article_picture.jpg");
-            FileInputStream fileInputStream = new FileInputStream(resource.getFile());
-            fileData = fileInputStream.readAllBytes();
+            fileData = resource.getInputStream().readAllBytes();
         } catch (IOException e) {
             e.printStackTrace();
         }
