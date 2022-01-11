43036ac96987021a52f80dcafafac20dda8e6a38
cry0x <kraemerj.schmelz@outlook.de>
2022-01-11 01:25:56 +0100

    fix

--- customer_service/src/main/java/com/customer_service/customer_service/CustomerServiceApplication.java
+++ customer_service/src/main/java/com/customer_service/customer_service/CustomerServiceApplication.java
@@ -2,6 +2,7 @@ package com.customer_service.customer_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 // import org.springframework.data.mongodb.repository.config.EnableMongoRepositorys;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
@@ -10,6 +11,7 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 // See: localhost:8080/swagger-ui.html
 // Get JSON: http://localhost:8080/v3/api-docs
 @OpenAPIDefinition
+@EnableEurekaClient
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
