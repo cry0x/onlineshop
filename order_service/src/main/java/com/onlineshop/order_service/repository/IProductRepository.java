da2f5074c9f796107a9ab0ee320857f9a60ab9f9
cry0x <kraemerj.schmelz@outlook.de>
2022-01-20 20:34:28 +0100

    fix build error

--- order_service/src/main/java/com/onlineshop/order_service/repository/IProductRepository.java
+++ order_service/src/main/java/com/onlineshop/order_service/repository/IProductRepository.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface IProductRepository extends JpaRepository<Product, Long> {
 
-    @Query("SELECT CASE WHEN COUNT(p) > 0 THEN true ELSE false END FROM products p WHERE p.original_id = :realProductId")
+    @Query("SELECT CASE WHEN COUNT(p) > 0 THEN true ELSE false END FROM Product p WHERE p.original_id = :realProductId")
     boolean existsProductByRealId(@Param("realProductId") Long realProductId);
 
 }
