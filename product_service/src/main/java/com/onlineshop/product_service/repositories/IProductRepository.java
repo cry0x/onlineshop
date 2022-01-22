072e441d78720ce0692a57170508011cdffd13b5
cry0x <kraemerj.schmelz@outlook.de>
2022-01-22 08:54:02 +0100

    pls be finished...

--- product_service/src/main/java/com/onlineshop/product_service/repositories/IProductRepository.java
+++ product_service/src/main/java/com/onlineshop/product_service/repositories/IProductRepository.java
@@ -5,8 +5,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+/**
+ * Interface which uses the standard-implementation of JpaRepository to make standard calls to the database without
+ * further need to implement the queries. Represents the Product table.
+ */
 public interface IProductRepository extends JpaRepository<Product, Long> {
 
+    /**
+     * This method represents a custom query which is used to get to know if a ProductPicture is still referenced inside
+     * a product.
+     * @param productPictureId
+     * @return
+     */
     @Query("SELECT CASE WHEN COUNT(p) > 0 THEN true ELSE false END FROM Product p WHERE p.productPicture.id = :productPictureId")
     boolean existsProductPictureInProduct(@Param("productPictureId") Long productPictureId);
 
